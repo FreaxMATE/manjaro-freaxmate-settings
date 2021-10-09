c13754e3cd0c42ccd010b68bc509189b8c7764a7
Konstantin Unruh <freaxmate@protonmail.com>
2021-10-10 01:27:58 +0200

    update

---
+++ scripts/freaxmate-config.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+## Script to set postinstall mate configs
+
+app=freaxmate-config
+
+volctl_dconf="[/]
+mixer-command='mate-volume-control'
+osd-enabled=false
+prefer-gtksi=false
+show-percentage=true
+vu-enabled=true
+"
+
+printf '%s\n' "$volctl_dconf" | dconf load /apps/volctl/
+
+# set standard shell to fish
+chsh -s /usr/bin/fish
+
+echo -e "export HISTCONTROL=ignoreboth:erasedups" >> /home/$USER/.bashrc
+
+gsettings set org.mate.panel.applet.window-list-previews:/org/mate/panel/objects/window-list/prefs/ show-window-thumbnails false
+gsettings set org.mate.panel.applet.workspace-switcher:/org/mate/panel/objects/object-1/prefs/ num-rows 2
+
+gsettings set org.mate.Marco.global-keybindings run-command-terminal "<Primary><Alt>t"
+gsettings set org.mate.control-center.keybinding:/org/mate/desktop/keybindings/custom0/ name 'Passwordsafe'
+gsettings set org.mate.control-center.keybinding:/org/mate/desktop/keybindings/custom0/ action 'gnome-passwordsafe'
+gsettings set org.mate.control-center.keybinding:/org/mate/desktop/keybindings/custom0/ binding '<Primary><Alt>s'
+
+mate-layouts --volumecontrol volctl
+
+#Remove the .desktop entry in HOME user folder
+
+rm $HOME/.config/autostart/$app.desktop
+
